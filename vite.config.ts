43641dd64e9a5bb81e169b13b162790c15fc81a3
Chanuka Devin <100866248+h2oalphaYT@users.noreply.github.com>
2026-04-20 10:01:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/chanukadev/', // 👈 Add this line
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
